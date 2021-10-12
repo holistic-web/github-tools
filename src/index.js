b487e46de1c98ca2dfe97a6efe79ad5b0e85b2f5
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-10-12 01:03:51 -0400

    fix: new run CLI appraoch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { program } = require('commander');
-const packageFile = require('./package.json');
-const commands = require('./src/commands');
+const packageFile = require('../package.json');
+const commands = require('./commands');
 
 program.version(packageFile.version);
 
